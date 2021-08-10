498e0ae6ce94acd0325b0bda2c0d65c45401b81e
Roshith R <r.roshith@gmail.com>
2021-08-10 19:32:17 +0530

    added id to texarea fields

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,18 @@
 import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
+
 import { useStyles } from '../utils/styles'
 
 export const Editor = () => {
  const classes = useStyles()
-  return <Paper className={classes.paper}>Editor</Paper>
+  return <Paper className={classes.paper}>
+    <TextField
+          id="editor"
+          label="Editor"
+          multiline
+          rows="7"
+          defaultValue="Default Value"
+          fullWidth
+        />
+  </Paper>
 }
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,7 +1,20 @@
 import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
+
 import { useStyles } from '../utils/styles'
 
 export const Preview = () => {
   const classes = useStyles()
-  return <Paper className={classes.paper}>Preview</Paper>
+  return (
+    <Paper className={classes.paper}>
+      <TextField
+        id='preview'
+        label='Preview'
+        multiline
+        rows='7'
+        defaultValue='Default Value'
+        fullWidth
+      />
+    </Paper>
+  )
 }
